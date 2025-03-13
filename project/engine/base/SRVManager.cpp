522adab47e72bc53e4d598deda35ad9aca0a6f85
sora <147121795+sora7716@users.noreply.github.com>
2025-03-13 12:08:57 +0900

    17ページまで進めた

--- project/engine/base/SRVManager.cpp
+++ project/engine/base/SRVManager.cpp
@@ -1,1 +1,65 @@
 #include "SRVManager.h"
+#include "DirectXBase.h"
+#include <cassert>
+
+//初期化
+void SRVManager::Initialize(DirectXBase* directXBase) {
+	//DirectXの基盤部分を記憶する
+	directXBase_ = directXBase;
+	//デスクリプタヒープの生成
+	descriptorHeap_ = directXBase_->MakeDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, kMaxSRVCount, true);
+	//デスクリプタ1個分のサイズを取得
+	descriptorSize_ = directXBase_->GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+}
+
+//確保
+uint32_t SRVManager::Allocate() {
+	//上限に達してないか？
+	assert(kMaxSRVCount < useIndex_);
+	//returnする番号を一旦記録しておく
+	int index = useIndex_;
+	//次回のために番号を1進める
+	useIndex_++;
+	//上で記録した番号をreturn
+	return index;
+}
+
+// SRV生成(テクスチャ用)
+void SRVManager::CreateSRVforTexture2D(uint32_t srvIndex, ID3D12Resource* resource, DXGI_FORMAT format, UINT mipLevels) {
+	//SRVの設定
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+	srvDesc.Format = format;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
+	srvDesc.Texture2D.MipLevels = UINT(mipLevels);
+	//設定を元にSRVを生成
+	directXBase_->GetDevice()->CreateShaderResourceView(resource, &srvDesc, GetCPUDescriptorHandle(srvIndex));
+}
+
+// SRV生成(Structured Buffer用)
+void SRVManager::CreateSRVforStructuredBuffer(uint32_t srvIndex, ID3D12Resource* resource, UINT numElements, UINT structureByteStride) {
+	// SRVの設定
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+	srvDesc.Format = DXGI_FORMAT_UNKNOWN; // Structured Buffer の場合は UNKNOWN
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_BUFFER; // バッファ用の設定
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.Buffer.NumElements = numElements;
+	srvDesc.Buffer.StructureByteStride = structureByteStride;
+	srvDesc.Buffer.Flags = D3D12_BUFFER_SRV_FLAG_NONE; // 必要なら RAW を設定
+	// 設定を元に SRV を生成
+	directXBase_->GetDevice()->CreateShaderResourceView(resource, &srvDesc, GetCPUDescriptorHandle(srvIndex));
+}
+
+// CPUデスクリプタハンドルのゲッター
+D3D12_CPU_DESCRIPTOR_HANDLE SRVManager::GetCPUDescriptorHandle(uint32_t index) {
+	D3D12_CPU_DESCRIPTOR_HANDLE handleCPU = descriptorHeap_->GetCPUDescriptorHandleForHeapStart();
+	handleCPU.ptr += (descriptorSize_ * index);
+	return handleCPU;
+}
+
+// GPUデスクリプタハンドルのゲッター
+D3D12_GPU_DESCRIPTOR_HANDLE SRVManager::GetGPUDescriptorHandle(uint32_t index) {
+	D3D12_GPU_DESCRIPTOR_HANDLE handleGPU = descriptorHeap_->GetGPUDescriptorHandleForHeapStart();
+	handleGPU.ptr += (descriptorSize_ * index);
+	return handleGPU;
+}
--- project/engine/base/SRVManager.h
+++ project/engine/base/SRVManager.h
@@ -1,5 +1,81 @@
 #pragma once
-class SRVManager
-{
+#include <stdint.h>
+#include <wrl.h>
+#include <d3d12.h>
+
+//前方宣言
+class DirectXBase;
+
+/// <summary>
+/// SRV管理
+/// </summary>
+class SRVManager{
+private://エイリアステンプレート
+	template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
+public://メンバ関数
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	SRVManager() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~SRVManager() = default;
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="directXBase">DirectXBaseの基盤部分</param>
+	void Initialize(DirectXBase*directXBase);
+	
+	/// <summary>
+	/// 確保
+	/// </summary>
+	/// <returns>インデックス</returns>
+	uint32_t Allocate();
+
+	/// <summary>
+	/// SRV生成(テクスチャ用)
+	/// </summary>
+	/// <param name="srvIndex">srvインデックス</param>
+	/// <param name="resource">リソース</param>
+	/// <param name="format">フォーマット</param>
+	/// <param name="mipLevels">ミップレベル</param>
+	void CreateSRVforTexture2D(uint32_t srvIndex, ID3D12Resource* resource, DXGI_FORMAT format, UINT mipLevels);
+
+	/// <summary>
+	/// SRV生成(Structured Buffer用)
+	/// </summary>
+	/// <param name="srvIndex">srvインデックス</param>
+	/// <param name="resource">リソース</param>
+	/// <param name="numElements">要素数</param>
+	/// <param name="structureByteStride"></param>
+	void CreateSRVforStructuredBuffer(uint32_t srvIndex, ID3D12Resource* resource, UINT numElements, UINT structureByteStride);
+
+	/// <summary>
+	/// CPUデスクリプタハンドルのゲッター
+	/// </summary>
+	/// <param name="index">インデックス</param>
+	/// <returns>CPUデスクリプタハンドル</returns>
+	D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(uint32_t index);
+
+	/// <summary>
+	/// GPUデスクリプタハンドルのゲッター
+	/// </summary>
+	/// <param name="index">インデックス</param>
+	/// <returns>GPUデスクリプタハンドル</returns>
+	D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle(uint32_t index);
+public://静的メンバ変数
+	static inline const uint32_t kMaxSRVCount = 512;
+private://メンバ変数
+	//DirectXの基盤部分
+	DirectXBase* directXBase_ = nullptr;
+	//SRV用のデスクリプタサイズ
+	uint32_t descriptorSize_ = 0;
+	//SRV用のデスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap>descriptorHeap_ = {};
+	//次に使用するSRVインデックス
+	uint32_t useIndex_ = 0;
 };
 
