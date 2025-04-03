6c98bf14ee9bc0b718924a70aa918aa088c2a72f
GSManager <GSManager@MSI>
2025-04-03 17:20:04 +0900

    スプライトの管理を作成中

---
+++ project/engine/2d/SpriteManager.cpp
@@ -0,0 +1,38 @@
+#include "SpriteManager.h"
+#include "Sprite.h"
+#include <cassert>
+
+//インスタンスのゲッター
+SpriteManager* SpriteManager::GetInstance() {
+	assert(!isFinalize && "GetInstance() called after Finalize()");
+	if (instance) {
+		instance = new SpriteManager();
+	}
+	return instance;
+}
+
+//スプライトの読み込み
+void SpriteManager::LoadSprite(const std::string& filePath) {
+	//読み込み済みならモデルを検索
+	if (sprites_.contains(filePath)) {
+		//読み込み済みなら早期return
+		return;
+	}
+	//モデルの生成とファイル読み込み、初期化
+	std::unique_ptr<Sprite>sprite = std::make_unique<Sprite>();
+	sprite->Initialize("engine/resources/"+filePath+);
+
+	//モデルをmapコンテナに格納する
+	sprites_.insert(std::make_pair(filePath, std::move(sprite)));
+}
+
+//スプライトの検索
+void SpriteManager::FindSprite(const std::string& filePath) {
+}
+
+//終了
+void SpriteManager::Finalize() {
+	delete instance;
+	instance = nullptr;
+	isFinalize = true;
+}
---
+++ project/engine/2d/SpriteManager.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <string>
+#include <map>
+#include <memory>
+
+//前方宣言
+class DirectXBase;
+class Sprite;
+
+/// <summary>
+/// スプライトの管理
+/// </summary>
+class SpriteManager{
+public://メンバ関数
+	/// <summary>
+	/// インスタンスのゲッター
+	/// </summary>
+	/// <returns>インスタンス</returns>
+	static SpriteManager* GetInstance();
+
+	/// <summary>
+	/// スプライトの読み込み
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void LoadSprite(const std::string& filePath);
+
+	/// <summary>
+	/// スプライトの検索
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void FindSprite(const std::string& filePath);
+
+	/// <summary>
+	/// 終了
+	/// </summary>
+	void Finalize();
+private://メンバ関数
+	//コンストラクタの封印
+	SpriteManager() = default;
+	//デストラクタの封印
+	~SpriteManager() = default;
+	//コピーコンストラクタの禁止
+	SpriteManager(const SpriteManager&) = delete;
+	//代入演算子の禁止
+	SpriteManager operator=(const SpriteManager&) = delete;
+private://静的メンバ変数
+	//インスタント
+	static inline SpriteManager* instance=nullptr;
+	//終了処理をしたかどういか
+	static inline bool isFinalize = false;
+private://メンバ変数
+	//スプライトたち
+	std::map<std::string, std::unique_ptr<Sprite>>sprites_;
+};
+
