b789ab5a61c0316cb7181367ca72d6c58282cf44
sora <147121795+sora7716@users.noreply.github.com>
2025-04-21 12:37:53 +0900

    2値抜きを作成した

--- project/engine/resources/shaders/Object3d.PS.hlsl
+++ project/engine/resources/shaders/Object3d.PS.hlsl
@@ -1,13 +1,13 @@
 #include "Object3d.hlsli"
 
-struct Material{
+struct Material {
     float4 color;
     int enableLighring;
     float4x4 uvTransform;
 };
 
 //平行光源
-struct DirectionalLight{
+struct DirectionalLight {
     float4 color; //ライトの色
     float3 direction; //ライトの向き
     float intensity; //輝度
@@ -18,16 +18,16 @@ Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 
-struct PixelShaderOutput{
+struct PixelShaderOutput {
     float4 color : SV_TARGET0;
 };
 
-PixelShaderOutput main(VertexShaderOutput input){
+PixelShaderOutput main(VertexShaderOutput input) {
     float4 transformedUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     PixelShaderOutput output;
     //Lightingする場合
-    if (gMaterial.enableLighring != 0){
+    if (gMaterial.enableLighring != 0) {
         //lambert
         // float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
         //half lambert
@@ -36,8 +36,20 @@ PixelShaderOutput main(VertexShaderOutput input){
         output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
         output.color.a = gMaterial.color.a * textureColor.a;
     }
-    else{
+    else {
         output.color = gMaterial.color * textureColor;
+    }
+    //textureのα値が0.5f以下の時にPixelを棄却
+    if (textureColor.a <= 0.5) {
+        discard;
+    }
+    //textureのα値の0の時にPixelを棄却
+    if (textureColor.a == 0.0) {
+        discard;
+    }
+    //output.colorのα値が0の時にPixelを棄却
+    if (output.color.a == 0.0) {
+        discard;
     }
     return output;
 }
