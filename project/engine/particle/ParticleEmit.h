4c9c8684a97471fe03b8a91f3e9039b933f6c38e
sora <147121795+sora7716@users.noreply.github.com>
2025-04-27 22:46:44 +0900

    セーブ

--- project/engine/particle/ParticleEmit.h
+++ project/engine/particle/ParticleEmit.h
@@ -1,13 +1,16 @@
 #pragma once
-#include "engine/math/func/Rendering.h"
-//前方宣言
-class DirectXBase;
-class Object2d;
+#include "engine/math/ResourceData.h"
+#include "engine/math/func/WorldTransform.h"
+#include <d3d12.h>
+#include <wrl.h>
+#include <memory>
 
 /// <summary>
 /// パーティクルのエミット
 /// </summary>
-class ParticleEmit{
+class ParticleEmit {
+private://エイリアステンプレート
+	template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
 public://メンバ関数
 	/// <summary>
 	/// コンストラクタ
@@ -39,12 +42,40 @@ public://メンバ関数
 	/// 終了
 	/// </summary>
 	void Finalize();
+private://メンバ関数
+	/// <summary>
+	/// モデルデータの初期化
+	/// </summary>
+	void InitializeModelData();
+
+	/// <summary>
+	/// マテリアルデータの初期化
+	/// </summary>
+	void InitializeMaterialData();
+
+	/// <summary>
+	/// 頂点リソースの生成
+	/// </summary>
+	void CreateVertexResource();
+
+	/// <summary>
+	/// マテリアルリソースの生成
+	/// </summary>
+	void CreateMaterialResource();
 private://メンバ変数
 	//DirectXの基盤部分
 	DirectXBase* directXBase_ = nullptr;
-	//オブジェクト2d
-	Object2d* object2d_ = nullptr;
 	//ワールドトランスフォーム
-	Transform2D worldTransform_ = {};
+	std::unique_ptr<WorldTransform>worldTransform_ = nullptr;
+	Transform transform_ = {};
+	//モデルデータ
+	ModelData modelData_ = {};
+	//マテリアルデータ
+	Material* materialData_ = nullptr;
+	//バッファリソース
+	ComPtr<ID3D12Resource>vertexResource_ = nullptr;//頂点
+	ComPtr<ID3D12Resource>materialResource_ = nullptr;//マテリアル
+	//バッファリソースの使い道を補足するバッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_ = {};//頂点
 };
 
