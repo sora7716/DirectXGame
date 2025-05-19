384be2248800adfdfa258c0b411cd159f007e837
sora <147121795+sora7716@users.noreply.github.com>
2025-05-19 19:42:37 +0900

    object3d_を描画した

--- project/engine/scene/TitleScene.cpp
+++ project/engine/scene/TitleScene.cpp
@@ -123,7 +123,7 @@ void TitleScene::Update() {
 
 //描画
 void TitleScene::Draw() {
-	//object3d_->Draw();
+	object3d_->Draw();
 	object2d_->Draw();
 	for (int i = 0; i < 2; i++) {
 		//plane_[i]->Draw();
